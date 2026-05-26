e85670f8fac2a71945622eb44c5fd153fb9d48dc
rianxkrzy <22400982@aluno.cotemig.com.br>
2026-05-26 11:17:48 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,72 +1,13 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, render_template, request
+from calculadora import calcular
 
 app = Flask(__name__)
 
-# Lista de usuários
-usuarios = [
-    {
-        "usuario": "rian",
-        "senha": "cotemig2026"
-    },
-    {
-        "usuario": "dolga",
-        "senha": "cotemig2026"
-    },
-    {
-        "usuario": "janaina",
-        "senha": "cotemig2026"
-    },
-    {
-        "usuario": "antonio",
-        "senha": "cotemig2026"
-    }
-]
-
-
-# Tela de login
-def mostrar_formulario():
-    return render_template_string("""
-        <h2>Login</h2>
-
-        <form method="POST">
-            <input type="text" name="usuario" placeholder="Usuário"><br><br>
-
-            <input type="password" name="senha" placeholder="Senha"><br><br>
-
-            <button type="submit">Entrar</button>
-        </form>
-    """)
-
-
-# Função de login
-def fazer_login():
-
-    usuario_digitado = request.form.get("usuario")
-    senha_digitada = request.form.get("senha")
-
-    # Percorrendo a lista de dicionários
-    for usuario in usuarios:
-
-        if (
-            usuario_digitado == usuario["usuario"]
-            and senha_digitada == usuario["senha"]
-        ):
-
-            return f"<h1>Bem-vindo, {usuario_digitado}!</h1>"
-
-    return "<h1>Login inválido!</h1>"
-
-
-# Rota principal
 @app.route("/", methods=["GET", "POST"])
-def login():
-
+def index():
     if request.method == "POST":
-        return fazer_login()
+        return calcular()
+    return render_template("calculadora.html", etpas="", resultados="")
 
-    return mostrar_formulario()
-
-
-# Executar aplicação
 if __name__ == "__main__":
     app.run(debug=True)
