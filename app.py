277e8374cc75b1c5d3b6d9dd5a7d18705005b8bc
rianxkrzy <22400982@aluno.cotemig.com.br>
2026-06-09 11:36:02 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template, request
-from calculadora import calcular
+import os
 
-app = Flask(__name__)
+from flask import Flask
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        return calcular()
-    return render_template("calculadora.html", etpas="", resultados="")
+from controllers import calculadora_bp
+from models import Operacao, db
+
+
+def criar_app():
+    app = Flask(
+        __name__,
+        template_folder="views/templates",
+        static_folder="views/static",
+    )
+
+    pasta_aula = os.path.abspath(os.path.dirname(__file__))
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+        pasta_aula, "calculadora.db"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    app.register_blueprint(calculadora_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
+app = criar_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
